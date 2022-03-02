24a5057eb9d7eb18c373f5511e17dd738aa3c1a9
Roger Vilà <rogervila@me.com>
2022-03-02 17:32:05 +0100

    Console "new" command (#12)

---
+++ equipment/console/tests/Commands/test_NewCommandTest.py
@@ -0,0 +1,36 @@
+import unittest
+from shutil import move
+from os import getcwd, sep
+from os.path import isdir, isfile
+from tempfile import gettempdir
+from datetime import datetime
+from codecs import open as _open
+from equipment.console.Commands.NewCommand import NewCommand
+
+
+class test_NewCommandTest(unittest.TestCase):
+    def test_command(self):
+        project = '__pycache__'  # ensure it is always ignored
+        full_path = f'{getcwd()}{sep}{project}'
+
+        if isdir(full_path):
+            move(full_path, f'{gettempdir()}{sep}{datetime.now()}{sep}{project}')  # nopep8
+
+        self.assertFalse(isdir(full_path))
+
+        self.assertIsNone(NewCommand(project).run())
+
+        self.assertTrue(isdir(full_path))
+
+        self.assertTrue(isfile(f'{full_path}{sep}.env'))
+
+        requirements_file = f'{full_path}{sep}requirements.txt'
+
+        self.assertTrue(isfile(requirements_file))
+
+        with _open(requirements_file, 'r') as f:
+            self.assertTrue('equipment==' in f.read())
+
+
+if __name__ == '__main__':
+    unittest.main()
