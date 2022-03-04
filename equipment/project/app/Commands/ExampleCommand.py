d3e5f38b4df4f9db4d5753a1696bf475c3a69fe1
Roger Vilà <rogervila@me.com>
2022-03-04 12:36:29 +0100

    Update ExampleCommand.py

--- equipment/project/app/Commands/ExampleCommand.py
+++ equipment/project/app/Commands/ExampleCommand.py
@@ -4,7 +4,7 @@ from equipment.console.Commands.AbstractCommand import AbstractCommand
 
 class ExampleCommand(AbstractCommand):
     def __init__(self) -> None:
-        pass
+        pass # Constructor is not extended from AbstractCommand. You are free to pass any parameter to it
 
     def run(self, *args, **kwargs) -> None:
         echo('Running example command!')
