2c46848feb5c76106ecaca4a992d78041c1d8f79
Roger Vilà <rogervila@me.com>
2022-03-21 11:33:07 +0100

    Container description (wip)

--- equipment/framework/App/Container.py
+++ equipment/framework/App/Container.py
@@ -7,9 +7,18 @@ from equipment.framework.Mail.MailFactory import MailFactory
 from equipment.framework.Queue.QueueFactory import QueueFactory
 from equipment.framework.Storage.StorageFactory import StorageFactory
 
+'''
+The Application Container uses dependency-injector
+under the hood.
+'''
+
 
 class Container(DeclarativeContainer):
+    '''The Application Container'''
+
     environment = Singleton(EnvironmentFactory)
+    '''The environment'''
+
     config = Singleton(ConfigFactory, environment)
     log = Singleton(LogFactory, config)
     queue = Singleton(QueueFactory, config, log)
