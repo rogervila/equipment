ac7dd45d582d5d67e8b363a628aadb1b81f3c683
rogervila <rogervila@me.com>
2025-01-02 14:11:40 +0100

    wip

--- Makefile
+++ Makefile
@@ -4,19 +4,6 @@ prepare:
 	rm -rf ./project/equipment
 	mkdir -p ./project/equipment
 	cp -r ./equipment/* ./project/equipment/
-	rm -rf ./equipment/Command/_project
-	mkdir -p ./equipment/Command/_project
-	cp -r ./project/* ./equipment/Command/_project/
-	rm -rf ./equipment/Command/_project/equipment
-	rm -rf ./equipment/Command/_project/htmlcov
-	cp project/.gitignore ./equipment/Command/_project/.gitignore
-	cp project/.env.example ./equipment/Command/_project/.env.example
-	cp project/.editorconfig ./equipment/Command/_project/.editorconfig
-	cp project/.coveragerc ./equipment/Command/_project/.coveragerc
-	cp project/database/.gitignore ./equipment/Command/_project/database/.gitignore
-	cp project/storage/app/.gitignore ./equipment/Command/_project/storage/app/.gitignore
-	cp project/storage/logs/.gitignore ./equipment/Command/_project/storage/logs/.gitignore
-	cp README.md ./equipment/Command/_project/README.md
 
 build:
 	make prepare
--- equipment/Command/NewProjectCommand.py
+++ equipment/Command/NewProjectCommand.py
@@ -3,14 +3,27 @@ from os.path import abspath, dirname, isdir, join
 from shutil import copyfile, copytree, ignore_patterns
 from click import confirm, echo, style
 from equipment.Command.AbstractCommand import AbstractCommand
-
+from requests import get
+from zipfile import ZipFile
+from io import BytesIO
 
 class NewProjectCommand(AbstractCommand):
     def run(self, name: str, path: str) -> None:
         project_template_path = f'{abspath(dirname(__file__))}{sep}_project'
 
         if not isdir(project_template_path):
-            echo(style(f'Fatal error: project template not found in "{project_template_path}". If the error persists, please open an issue', fg='red'))
+            try:
+                response = get('https://github.com/rogervila/equipment/archive/refs/heads/main.zip')
+                with ZipFile(BytesIO(response.content)) as zip_file:
+                    zip_file.extractall(project_template_path)
+            except Exception as e:
+                echo(style(f'Could not download project template: {e}', fg='red'))
+                return
+
+        project_template_path = f'{project_template_path}{sep}equipment-main{sep}project'
+
+        if not isdir(project_template_path):
+            echo(style(f'Project template not found in "{project_template_path}". If the error persists, please open an issue', fg='red'))
             return
 
         try:
