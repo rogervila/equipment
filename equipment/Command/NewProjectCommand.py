cf168419e34887e085370ae921770d659a8d95e8
rogervila <rogervila@me.com>
2025-01-02 15:00:01 +0100

    Avoid cache project template

--- equipment/Command/NewProjectCommand.py
+++ equipment/Command/NewProjectCommand.py
@@ -1,6 +1,6 @@
 from os import sep
 from os.path import abspath, dirname, isdir, join
-from shutil import copyfile, copytree, ignore_patterns
+from shutil import copyfile, copytree, ignore_patterns, rmtree
 from click import confirm, echo, style
 from equipment.Command.AbstractCommand import AbstractCommand
 from requests import get
@@ -11,14 +11,16 @@ class NewProjectCommand(AbstractCommand):
     def run(self, name: str, path: str) -> None:
         project_template_path = f'{abspath(dirname(__file__))}{sep}_project'
 
-        if not isdir(project_template_path):
-            try:
-                response = get('https://github.com/rogervila/equipment/archive/refs/heads/main.zip')
-                with ZipFile(BytesIO(response.content)) as zip_file:
-                    zip_file.extractall(project_template_path)
-            except Exception as e:
-                echo(style(f'Could not download project template: {e}', fg='red'))
-                return
+        if isdir(project_template_path):
+            rmtree(project_template_path)
+
+        try:
+            response = get('https://github.com/rogervila/equipment/archive/refs/heads/main.zip')
+            with ZipFile(BytesIO(response.content)) as zip_file:
+                zip_file.extractall(project_template_path)
+        except Exception as e:
+            echo(style(f'Could not download project template: {e}', fg='red'))
+            return
 
         project_template_path = f'{project_template_path}{sep}equipment-main{sep}project'
 
