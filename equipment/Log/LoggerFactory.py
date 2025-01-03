f56eacf5f2b371187909de419c8d0d2d6b744aa1
rogervila <rogervila@me.com>
2025-01-03 10:47:40 +0100

    Log formatter per channel

--- equipment/Log/LoggerFactory.py
+++ equipment/Log/LoggerFactory.py
@@ -8,24 +8,25 @@ from pythonjsonlogger.jsonlogger import JsonFormatter
 class LoggerFactory(AbstractLogger):
     logger: Logger
     base_path: str
+    config: dict
 
     def __init__(self, name: str, base_path: str, config: dict):
         self.base_path = base_path
+        self.config = config
 
         if not hasattr(self, 'logger'):
             logger = getLogger(name)
             _clear_logger(logger)
 
             logger.setLevel(
-                getLevelNamesMapping()[str(config['level']).upper().strip()]
+                getLevelNamesMapping()[str(
+                    self.config['level']).upper().strip()
+                ]
             )
 
-            handlers = self._get_handlers_list(config)
+            handlers = self._get_handlers_list()
 
             for handler in handlers:
-                if 'formatter' in config and str(config['formatter']).lower() == 'json':
-                    handler.setFormatter(JsonFormatter(config['formatters']['json']['format'], json_indent=None))
-
                 logger.addHandler(handler)
 
             self.logger = logger
@@ -45,42 +46,55 @@ class LoggerFactory(AbstractLogger):
     def critical(self, *args, **kwargs) -> None:
         self.logger.critical(*args, **kwargs)
 
-    def _get_handlers_list(self, config: dict) -> list[Handler]:
-        channel = config['channel']
+    def _get_handlers_list(self) -> list[Handler]:
+        channel = self.config['channel']
 
         if channel is None or channel == 'None' or channel == 'null':
             return [NullHandler()]
 
-        if channel == 'stack':
-            handlers = []
-            channels = list(config['channels']['stack']['channels'])
+        handlers = []
+        channels = list(
+            self.config['channels']['stack']['channels']
+        ) if channel == 'stack' else [channel]
 
-            for channel in channels:
-                handlers.append(self._get_channel_handler(channel, config['channels'][channel]))
-            return handlers
+        for channel in channels:
+            handlers.append(
+                self._get_channel_handler(
+                    channel,
+                    self.config['channels'][channel]
+                )
+            )
 
-        return [self._get_channel_handler(channel, config['channels'][channel])]
+        return handlers
 
-    def _get_channel_handler(self, channel: str, config: dict) -> Handler:
+    def _get_channel_handler(self, channel: str, channel_config: dict) -> Handler:
         if channel == 'single':
             handler = FileHandler(os.path.join(
                 self.base_path,
-                str(config['filename'])
+                str(channel_config['filename'])
             ))
         elif channel == 'daily':
             handler = TimedRotatingFileHandler(
                 filename=os.path.join(
                     self.base_path,
-                    str(config['filename'])
+                    str(channel_config['filename'])
                 ),
-                when=str(config['when']),
-                interval=int(config['interval']),
-                backupCount=int(config['backupCount'])
+                when=str(channel_config['when']),
+                interval=int(channel_config['interval']),
+                backupCount=int(channel_config['backupCount'])
             )
         elif channel == 'console':
-            handler = StreamHandler(config['stream'])
+            handler = StreamHandler(channel_config['stream'])
         else:
             raise ValueError(f'Invalid channel name: {channel}')
+
+        if 'formatter' in channel_config and str(channel_config['formatter']).strip().lower() == 'json':
+            handler.setFormatter(
+                JsonFormatter(
+                    self.config['formatters']['json']['format'],
+                    json_indent=self.config['formatters']['json']['indent']
+                )
+            )
 
         return handler
 
