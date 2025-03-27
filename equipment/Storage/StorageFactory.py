cff5e85d063e6be5733b86f2e56a4cae3776b15a
rogervila <rogervila@me.com>
2025-03-27 13:08:56 +0100

    S3 Storage Driver

--- equipment/Storage/StorageFactory.py
+++ equipment/Storage/StorageFactory.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING
 from equipment.Storage.AbstractStorage import AbstractStorage
 from equipment.Storage.LocalStorage import LocalStorage
+from equipment.Storage.S3Storage import S3Storage
 
 if TYPE_CHECKING:
     from equipment.Log.AbstractLogger import AbstractLogger
@@ -12,12 +13,15 @@ class StorageFactory(AbstractStorage):
     def __init__(self, config: dict, base_path: str, log: 'AbstractLogger'):
         if not hasattr(self, 'storage'):
             if config['disk'] == 'local':
-                self.storage = LocalStorage(config['disks']['local'], base_path, log)
-                return
-
-            # TODO: S3Storage, FTPStorage, etc.
-
-            raise ValueError(f'Unknown storage type type: {config["disk"]}')
+                self.storage = LocalStorage(
+                    config['disks']['local'],
+                    base_path,
+                    log
+                )
+            elif config['disk'] == 's3':
+                self.storage = S3Storage(config['disks']['s3'], log)
+            else:
+                raise ValueError(f'Unknown storage type: {config["disk"]}')
 
     def path(self, file: str) -> str:
         return self.storage.path(file)
